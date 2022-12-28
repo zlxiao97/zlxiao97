33a9d494e6c831ac5798dc066e9e528802f966ed
肖子立 <xiaozlb@digitalchina.com>
2022-12-28 17:31:37 +0800

    feat: 🎸 add vuepress site

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+import { defineUserConfig } from 'vuepress'
+
+export default defineUserConfig({
+  base: '/',
+  locales: {
+    '/': {
+      lang: 'zh-CN',
+      title: 'zlxiao97',
+      description: 'Welcome to my VuePress site',
+    },
+  },
+})
